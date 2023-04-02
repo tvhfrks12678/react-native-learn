e1b31cba57f16b4d2ec2055de19e08ffa8184017
tvhfrks12678 <tvhfrks12678@gmail.com>
2023-04-02 11:59:26 +0900

    :technologist: learn guides design style

--- App.js
+++ App.js
@@ -3,13 +3,25 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>11Try editing me!</Text>
+    <View style={style.container}>
+      <Text style={style.red}>just red</Text>
+      <Text style={style.bigBlue}>just bigBlue</Text>
+      <Text style={[style.bigBlue, style.red]}>bigBlue, then red</Text>
+      <Text style={[style.red, style.bigBlue]}>red, then bigBlue</Text>
     </View>
   );
 }
+
+const style = StyleSheet.create({
+  container: {
+    marginTop: 50,
+  },
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+});
